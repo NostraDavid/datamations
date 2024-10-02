dd084efb2dc1edc4df399c623d597084a8cbf553
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2024-10-02 13:10:27 +0200

    Rip out anything that doesn't vaguely smell like Python

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-X_FIELD_CHR <- "datamations_x"
-X_FIELD <- rlang::sym(X_FIELD_CHR)
-Y_FIELD_CHR <- "datamations_y"
-Y_FIELD <- rlang::sym(Y_FIELD_CHR)
-Y_RAW_FIELD_CHR <- "datamations_y_raw"
-Y_RAW_FIELD <- rlang::sym(Y_RAW_FIELD_CHR)
-Y_TOOLTIP_FIELD_CHR <- "datamations_y_tooltip"
-Y_TOOLTIP_FIELD <- rlang::sym(Y_TOOLTIP_FIELD_CHR)
--- data-raw/small_salary.R
+++
@@ -1,5 +0,0 @@
-library(readr)
-
-small_salary <- read_csv(here::here("data-raw", "small_salary.csv"))
-
-usethis::use_data(small_salary, overwrite = TRUE)
